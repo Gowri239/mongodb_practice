8e6e407bd3a1a3707847f87e100bf2cda0d8e4eb
Gowri239 <gowrimopuru14@gmail.com>
2022-12-01 12:09:49 +0530

    creating users table

--- models/user.js
+++ models/user.js
@@ -1,16 +1,26 @@
-const Sequelize = require('sequelize');
+const monodb = require('mongodb');
 
-const sequelize = require('../util/database');
+const ObjectId = monodb.ObjectId;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+const getDb = require('../util/database').getDb;
+
+class User{
+  constructor(username,email){
+    this.name = username;
+    this.email = email;
+  }
+  save(){
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
+
+  static findById(userId){
+    const db = getDb();
+    return db.collection('users').findOne({_id : new ObjectId(userId)})
+
+  }
+
+
+}
 
 module.exports = User;
